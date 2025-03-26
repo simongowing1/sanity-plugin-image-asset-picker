bca740e71049b3965d14c6bb4ef7bbd987f1f916
simongowing1 <simongowing1@gmail.com>
2025-03-26 13:06:40 +0100

    refactor: wrap picker in dialogue box

--- src/ImageAssetPicker.tsx
+++ src/ImageAssetPicker.tsx
@@ -1,6 +1,6 @@
 import { SanityAsset } from '@sanity/image-url/lib/types/types';
-import { Button, Card, Checkbox, Flex, Grid, Inline, Stack, Text, TextInput } from '@sanity/ui';
-import { FloppyDisk } from 'phosphor-react';
+import { Button, Card, Checkbox, Dialog, Flex, Grid, Inline, Stack, Text, TextInput } from '@sanity/ui';
+import { FloppyDisk, FolderSimplePlus } from 'phosphor-react';
 import { FormEvent, useEffect, useMemo, useState } from 'react';
 import { ArrayOfObjectsInputProps, insert, setIfMissing, useClient } from 'sanity';
 
@@ -12,7 +12,9 @@ export function ImageAssetPicker({ ...props }: Props) {
     const client = useClient({ apiVersion: '2022-11-15' });
 
     const [imageAssets, setImageAssets] = useState<SanityAsset[]>([]);
-    const [loading, setLoading] = useState(true);
+    const [imageAssetsLoading, setImageAssetsLoading] = useState(true);
+
+    const [isImageAssetDialogueOpen, setIsImageAssetDialogueOpen] = useState(false);
     const [selectedImageAssets, setSelectedImageAssets] = useState<Set<SanityAsset>>(new Set());
     const selectedImageAssetsArray = useMemo(() => [...selectedImageAssets], [selectedImageAssets]);
 
@@ -45,7 +47,7 @@ export function ImageAssetPicker({ ...props }: Props) {
             } else {
                 newSet.add(image)
             }
-            return newSet
+            return newSet;
         })
     };
 
@@ -58,13 +60,14 @@ export function ImageAssetPicker({ ...props }: Props) {
 
         const imagePatches = savedImageAssets.map((image) => insert([image], 'after', [-1]))
 
-        onChange([setIfMissing([]), ...imagePatches])
-        setSelectedImageAssets(new Set())
+        onChange([setIfMissing([]), ...imagePatches]);
+        setSelectedImageAssets(new Set());
+        setIsImageAssetDialogueOpen(false);
     };
 
     function handleSearchInputSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault()
-        setLoading(true)
+        setImageAssetsLoading(true)
         const totalQuery = `count(${imageAssetQueryWithSearchString})`
         client.fetch(totalQuery, { searchString: searchString }).then((totalCount: number) => {
             setTotalPages(Math.ceil(totalCount / pageSize))
@@ -72,7 +75,7 @@ export function ImageAssetPicker({ ...props }: Props) {
         const query = imageAssetQueryBuilder(imageAssetQueryWithSearchString, 1, pageSize)
         client.fetch(query, { searchString: searchString }).then((images: SanityAsset[]) => {
             setImageAssets(images)
-            setLoading(false)
+            setImageAssetsLoading(false)
         })
     };
 
@@ -85,6 +88,11 @@ export function ImageAssetPicker({ ...props }: Props) {
         toggleImageAsset(image)
     };
 
+    const handleCloseImageAssetDialogue = () => {
+        setIsImageAssetDialogueOpen(false)
+        setSelectedImageAssets(new Set())
+    }
+
     useEffect(() => {
         const totalQuery = `count(${imageAssetQueryWithoutSearchString})`
         client.fetch(totalQuery).then((totalCount: number) => {
@@ -94,7 +102,7 @@ export function ImageAssetPicker({ ...props }: Props) {
     }, []);
 
     useEffect(() => {
-        setLoading(true)
+        setImageAssetsLoading(true)
         const start = (page - 1) * pageSize
         const end = start + pageSize
         const query = searchString
@@ -102,110 +110,122 @@ export function ImageAssetPicker({ ...props }: Props) {
             : imageAssetQueryBuilder(imageAssetQueryWithoutSearchString, start, end)
         client.fetch(query, { searchString: searchString }).then((images: SanityAsset[]) => {
             setImageAssets(images)
-            setLoading(false)
+            setImageAssetsLoading(false)
         })
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [page, pageSize]);
+    }, [page, pageSize, props.value]);
 
     return (
         <Stack space={6}>
-            {props.renderDefault(props)}
-            <Stack space={4}>
-                <Text size={1} style={{ fontWeight: '500' }}>
-                    Images
-                </Text>
-                <Stack space={6}>
-                    <form onSubmit={handleSearchInputSubmit}>
-                        <Flex direction={'column'} gap={2}>
-                            <TextInput
-                                width={'fill'}
-                                type="text"
-                                value={searchString || ''}
-                                onChange={handleSearchInputChange}
-                                placeholder="Search for images"
-                            />
-                            <Button type="submit" mode="ghost" padding={3} width={'fill'}>
-                                <Flex justify="center" align="center">
-                                    <Text size={1}>Search</Text>
-                                </Flex>
-                            </Button>
-                        </Flex>
-                    </form>
-
-                    <Stack space={4}>
-                        <Button onClick={handleSaveImagesClick} mode="ghost" padding={3} width={'fill'}>
-                            <Flex justify="center" align="center">
-                                <Inline space={2}>
-                                    <FloppyDisk size={16} />
-                                    <Text size={1}>Save selected images</Text>
-                                </Inline>
-                            </Flex>
-                        </Button>
-                        {loading ? (
-                            <Text size={1}>Loading images...</Text>
-                        ) : imageAssets.length === 0 ? (
-                            <Text size={1}>No images found</Text>
-                        ) : (
-                            <Stack space={2}>
-                                <Grid columns={4} rows={Math.ceil(imageAssets.length / 4)} gap={1}>
-                                    {imageAssets.map((image, index) => (
-                                        <Card
-                                            key={image._id || index}
-                                            padding={1}
-                                            radius={2}
-                                            tone="default"
-                                            value={image._id}
-                                        >
-                                            <Stack space={3}>
-                                                <Flex justify="space-between" align="center">
-                                                    <Checkbox
-                                                        id={image._id}
-                                                        checked={selectedImageAssets.has(image)}
-                                                        onChange={() => handleCheckboxChange(image)}
-                                                    />
-                                                    <Text size={1} muted>
-                                                        {selectedImageAssets.has(image.uploadId) ? 'Selected' : 'Not selected'}
-                                                    </Text>
-                                                </Flex>
-                                                {image.url && (
-                                                    <img
-                                                        src={`${image.url}?w=200`}
-                                                        alt={image.title}
-                                                        className="h-[150px] w-full object-cover"
-                                                    />
-                                                )}
-                                                <Stack space={2}>
-                                                    <Text size={1}>{image.title}</Text>
-                                                    {image.artistName && (
-                                                        <Text size={1} muted>
-                                                            by {image.artistName}
-                                                        </Text>
-                                                    )}
-                                                </Stack>
-                                            </Stack>
-                                        </Card>
-                                    ))}
-                                </Grid>
-                                <Grid columns={12} rows={totalPages / 12} gap={1}>
-                                    {[...Array(totalPages)].map((_, index) => (
-                                        <Button
-                                            key={index}
-                                            mode="ghost"
-                                            padding={3}
-                                            width={'fill'}
-                                            onClick={() => setPage(index + 1)}
-                                        >
-                                            <Flex justify="center" align="center">
-                                                <Text size={1}>{index + 1}</Text>
-                                            </Flex>
-                                        </Button>
-                                    ))}
-                                </Grid>
-                            </Stack>
-                        )}
-                    </Stack>
-                </Stack>
+            <Stack space={2}>
+                {props.renderDefault(props)}
+                <Button mode="ghost" padding={2} width={'fill'} onClick={() => setIsImageAssetDialogueOpen(true)}>
+                    <Flex justify="center" align="center">
+                        <Inline space={3}>
+                            <FolderSimplePlus size={20} />
+                            <Text size={1}>Select Uploaded Images</Text>
+                        </Inline>
+                    </Flex>
+                </Button>
             </Stack>
+
+            {isImageAssetDialogueOpen &&
+                <Dialog id="image-asset-dialogue" width={100} header="Uploaded Images" onClose={handleCloseImageAssetDialogue}>
+                    <Stack space={4} padding={4}>
+                        <Stack space={6}>
+                            <form onSubmit={handleSearchInputSubmit}>
+                                <Flex direction={'column'} gap={2}>
+                                    <TextInput
+                                        width={'fill'}
+                                        type="text"
+                                        value={searchString || ''}
+                                        onChange={handleSearchInputChange}
+                                        placeholder="Search for images by filename"
+                                    />
+                                    <Button type="submit" mode="ghost" padding={3} width={'fill'}>
+                                        <Flex justify="center" align="center">
+                                            <Text size={1}>Search</Text>
+                                        </Flex>
+                                    </Button>
+                                </Flex>
+                            </form>
+
+                            <Stack space={4}>
+                                <Button onClick={handleSaveImagesClick} mode="ghost" padding={2} width={'fill'}>
+                                    <Flex justify="center" align="center">
+                                        <Inline space={3}>
+                                            <FloppyDisk size={20} />
+                                            <Text size={1}>Save selected images</Text>
+                                        </Inline>
+                                    </Flex>
+                                </Button>
+                                {imageAssetsLoading ? (
+                                    <Text size={1}>Loading images...</Text>
+                                ) : imageAssets.length === 0 ? (
+                                    <Text size={1}>No images found</Text>
+                                ) : (
+                                    <Stack space={2}>
+                                        <Grid columns={4} rows={Math.ceil(imageAssets.length / 4)} gap={1}>
+                                            {imageAssets.map((image, index) => (
+                                                <Card
+                                                    key={image._id || index}
+                                                    padding={1}
+                                                    radius={2}
+                                                    tone="default"
+                                                    value={image._id}
+                                                >
+                                                    <Stack space={3}>
+                                                        <Flex justify="space-between" align="center">
+                                                            <Checkbox
+                                                                id={image._id}
+                                                                checked={selectedImageAssets.has(image)}
+                                                                onChange={() => handleCheckboxChange(image)}
+                                                            />
+                                                            <Text size={1} muted>
+                                                                {selectedImageAssets.has(image.uploadId) ? 'Selected' : 'Not selected'}
+                                                            </Text>
+                                                        </Flex>
+                                                        {image.url && (
+                                                            <img
+                                                                src={`${image.url}?w=200`}
+                                                                alt={image.title}
+                                                                className="h-[150px] w-full object-cover"
+                                                            />
+                                                        )}
+                                                        <Stack space={2}>
+                                                            <Text size={1}>{image.title}</Text>
+                                                            {image.artistName && (
+                                                                <Text size={1} muted>
+                                                                    by {image.artistName}
+                                                                </Text>
+                                                            )}
+                                                        </Stack>
+                                                    </Stack>
+                                                </Card>
+                                            ))}
+                                        </Grid>
+                                        <Grid columns={12} rows={totalPages / 12} gap={1}>
+                                            {[...Array(totalPages)].map((_, index) => (
+                                                <Button
+                                                    key={index}
+                                                    mode="ghost"
+                                                    padding={3}
+                                                    width={'fill'}
+                                                    onClick={() => setPage(index + 1)}
+                                                >
+                                                    <Flex justify="center" align="center">
+                                                        <Text size={1}>{index + 1}</Text>
+                                                    </Flex>
+                                                </Button>
+                                            ))}
+                                        </Grid>
+                                    </Stack>
+                                )}
+                            </Stack>
+                        </Stack>
+                    </Stack>
+                </Dialog>
+            }
         </Stack>
     )
 };
