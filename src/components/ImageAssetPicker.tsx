1f845f18e2f9c5e55a7ad2dca471e6e6e53891ec
simongowing1 <simongowing1@gmail.com>
2025-03-26 14:04:19 +0100

    refactor: instigate useImageAssetPicker hook

--- src/components/ImageAssetPicker.tsx
+++ src/components/ImageAssetPicker.tsx
@@ -1,107 +1,29 @@
-import { SanityAsset } from '@sanity/image-url/lib/types/types';
-import { Button, Card, Checkbox, Dialog, Flex, Grid, Inline, Stack, Text, TextInput } from '@sanity/ui';
+import { Button, Dialog, Flex, Grid, Inline, Stack, Text, TextInput } from '@sanity/ui';
 import { FloppyDisk, FolderSimplePlus } from 'phosphor-react';
-import { FormEvent, useEffect, useMemo, useState } from 'react';
-import { ArrayOfObjectsInputProps, insert, setIfMissing, useClient } from 'sanity';
-import { imageAssetQueryBuilder, imageAssetQueryWithSearchString, imageAssetQueryWithoutSearchString } from '../../utils/helpers/queryHelpers';
+import { ArrayOfObjectsInputProps } from 'sanity';
 import { ImageAssetCard } from './ImageAssetCard';
+import { useImageAssetPicker } from '../../utils/hooks/useImageAssetPicker';
 
 type Props = ArrayOfObjectsInputProps
 
 export function ImageAssetPicker({ ...props }: Props) {
     const { onChange } = props;
 
-    const client = useClient({ apiVersion: '2022-11-15' });
-
-    const [imageAssets, setImageAssets] = useState<SanityAsset[]>([]);
-    const [imageAssetsLoading, setImageAssetsLoading] = useState(true);
-
-    const [isImageAssetDialogueOpen, setIsImageAssetDialogueOpen] = useState(false);
-    const [selectedImageAssets, setSelectedImageAssets] = useState<Set<SanityAsset>>(new Set());
-    const selectedImageAssetsArray = useMemo(() => [...selectedImageAssets], [selectedImageAssets]);
-
-    const [page, setPage] = useState<number>(1);
-    const [pageSize] = useState<number>(80);
-    const [totalPages, setTotalPages] = useState<number>(0);
-
-    const [searchString, setSearchString] = useState<string | null>(null);
-
-    const toggleImageAsset = async (image: SanityAsset) => {
-        setSelectedImageAssets((prev) => {
-            const newSet = new Set(prev)
-            if (newSet.has(image)) {
-                newSet.delete(image)
-            } else {
-                newSet.add(image)
-            }
-            return newSet;
-        })
-    };
-
-    const handleSaveImagesClick = () => {
-        const savedImageAssets: SanityAsset[] = selectedImageAssetsArray.map((image) => ({
-            _key: image._id,
-            type: `image`,
-            asset: { _ref: image._id, _type: 'reference' },
-        }))
-
-        const imagePatches = savedImageAssets.map((image) => insert([image], 'after', [-1]))
-
-        onChange([setIfMissing([]), ...imagePatches]);
-        setSelectedImageAssets(new Set());
-        setIsImageAssetDialogueOpen(false);
-    };
-
-    function handleSearchInputSubmit(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault()
-        setImageAssetsLoading(true)
-        const totalQuery = `count(${imageAssetQueryWithSearchString})`
-        client.fetch(totalQuery, { searchString: searchString }).then((totalCount: number) => {
-            setTotalPages(Math.ceil(totalCount / pageSize))
-        })
-        const query = imageAssetQueryBuilder(imageAssetQueryWithSearchString, 1, pageSize)
-        client.fetch(query, { searchString: searchString }).then((images: SanityAsset[]) => {
-            setImageAssets(images)
-            setImageAssetsLoading(false)
-        })
-    };
-
-    const handleSearchInputChange = (event: FormEvent<HTMLInputElement>) => {
-        const nextValue = event.currentTarget.value
-        setSearchString(nextValue)
-    };
-
-    const handleCheckboxChange = (image: SanityAsset) => {
-        toggleImageAsset(image)
-    };
-
-    const handleCloseImageAssetDialogue = () => {
-        setIsImageAssetDialogueOpen(false)
-        setSelectedImageAssets(new Set())
-    }
-
-    useEffect(() => {
-        const totalQuery = `count(${imageAssetQueryWithoutSearchString})`
-        client.fetch(totalQuery).then((totalCount: number) => {
-            setTotalPages(Math.ceil(totalCount / pageSize))
-        })
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    useEffect(() => {
-        if (!isImageAssetDialogueOpen) return;
-        setImageAssetsLoading(true)
-        const start = (page - 1) * pageSize
-        const end = start + pageSize
-        const query = searchString
-            ? imageAssetQueryBuilder(imageAssetQueryWithSearchString, start, end)
-            : imageAssetQueryBuilder(imageAssetQueryWithoutSearchString, start, end)
-        client.fetch(query, { searchString: searchString }).then((images: SanityAsset[]) => {
-            setImageAssets(images)
-            setImageAssetsLoading(false)
-        })
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [page, pageSize, isImageAssetDialogueOpen]);
+    const {
+        imageAssets,
+        imageAssetsLoading,
+        isImageAssetDialogueOpen,
+        selectedImageAssets,
+        totalPages,
+        handleSaveImagesClick,
+        handleSearchInputSubmit,
+        handleSearchInputChange,
+        handleCheckboxChange,
+        handleCloseImageAssetDialogue,
+        setPage,
+        setIsImageAssetDialogueOpen,
+        searchString,
+    } = useImageAssetPicker({ onChange });
 
     return (
         <Stack space={6}>
