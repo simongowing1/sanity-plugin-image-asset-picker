e08d33bb223b0706c1fd3b6ec93772b290b5d23a
simongowing1 <simongowing1@gmail.com>
2025-03-27 15:00:01 +0100

    style: ImageAssetGrid

--- src/components/ImageAssetGrid.tsx
+++ src/components/ImageAssetGrid.tsx
@@ -1,11 +1,10 @@
-import { Flex, Inline, Text } from "@sanity/ui"
+import { Flex, Spinner, Text } from "@sanity/ui"
 import { Grid } from "@sanity/ui"
 import { Button } from "@sanity/ui"
 import { Stack } from "@sanity/ui"
-import { FloppyDisk } from "phosphor-react"
 import { ImageAssetCard } from "./ImageAssetCard"
 import { SanityAsset } from "@sanity/image-url/lib/types/types"
-
+import { AddIcon } from "@sanity/icons"
 interface ImageAssetGridProps {
     imageAssets: SanityAsset[];
     selectedImageAssets: Set<SanityAsset>;
@@ -20,17 +19,21 @@ export const ImageAssetGrid = (props: ImageAssetGridProps) => {
     const { imageAssets, selectedImageAssets, onSave, onCheckboxChange, onPageChange, totalPages, imageAssetsLoading } = props;
     return (
         <Stack space={4}>
-            <Button onClick={onSave} mode="ghost" padding={2} width={'fill'}>
-                <Flex justify="center" align="center">
-                    <Inline space={3}>
-                        <FloppyDisk size={20} />
-                        <Text size={1}>Save selected images</Text>
-                    </Inline>
-                </Flex>
-            </Button>
-
+            <Button
+                mode="ghost"
+                paddingY={2}
+                width={'fill'}
+                onClick={onSave}
+                icon={AddIcon}
+                text="Insert Selected Images"
+            />
             {imageAssetsLoading ? (
-                <Text size={1}>Loading images...</Text>
+                <Flex align="center" gap={2} paddingY={2}>
+                    <Spinner muted />
+                    <Text size={1} muted>
+                        Loading images...
+                    </Text>
+                </Flex>
             ) : imageAssets.length === 0 ? (
                 <Text size={1}>No images found</Text>
             ) : (
